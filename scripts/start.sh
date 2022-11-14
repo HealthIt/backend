94c0ce0265be93f4c7e89cdc7407a5ebad42c9cf
pcy <cas15987@gmail.com>
2022-11-14 16:46:14 +0900

    rename: sh 파일명 변경
    deploy.sh -> start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,9 +9,9 @@ TIME_NOW=$(date +%c)
 
 # 실행중인 애플리케이션 종료
 JAR_FILE=$(ls -tr $REPOSITORY/*.jar)
-echo "$TIME_NOW > jar 파일 확인: $JAR_FILE" > DEPLOY_LOG
+echo "$TIME_NOW > jar 파일 확인: $JAR_FILE" >> DEPLOY_LOG
 
-echo "$TIME_NOW > 현재 구동 중인 애플리케이션 pid 확인"
+echo "$TIME_NOW > 현재 구동 중인 애플리케이션 pid 확인" >> DEPLOY_LOG
 CURRENT_PID=$(pgrep -f "$JAR_FILE")
 
 if [ -z "$CURRENT_PID" ]; then
@@ -22,13 +22,13 @@ else
 fi
 
 # 애플리케이션 실행
-echo "$TIME_NOW >  $JAR_FILE 에 실행권한 추가" > DEPLOY_LOG
+echo "$TIME_NOW > $JAR_FILE 에 실행권한 추가" >> $DEPLOY_LOG
 
 chmod +x "$JAR_FILE"
 
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 
-nohup java -jar -Duser.timezone=Asia/Seoul "$JAR_FILE" >> $APP_LOG 2> ERROR_LOG &
+nohup java -jar -Duser.timezone=Asia/Seoul "$JAR_FILE" > $APP_LOG 2> ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f "$JAR_FILE")
 echo "$TIME_NOW > 프로세스 아이디: $CURRENT_PID" >> $DEPLOY_LOG
